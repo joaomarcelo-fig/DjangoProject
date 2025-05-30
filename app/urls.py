241616dc797d405806b60149b6a112ea21f58dc1
vinin <vininobre31@gmail.com>
2025-05-29 21:24:31 -0400

    CRUD category

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
-    path('new/', views.item_create, name='item_create'),
-    path('edit/<int:pk>/', views.item_update, name='item_update'),
-    path('delete/<int:pk>/', views.item_delete, name='item_delete'),
-]
+    path('', views.category_list, name='category_list'),
+    path('new/category', views.category_create, name='category_create'),
+    path('edit/category/<int:pk>/', views.category_update, name='category_update'),
+    path('delete/category/<int:pk>/', views.category_delete, name='category_delete'),
+    #path('', views.item_list, name='item_list'),
+    path('new/item', views.item_create, name='item_create'),
+    path('edit/item/<int:pk>/', views.item_update, name='item_update'),
+    path('delete/item/<int:pk>/', views.item_delete, name='item_delete'),
+
+ ]
